1a30dbec7c46ccc38088f3f6d855874510c4c034
Hadeed711 <hadeedahmad711@gmail.com>
2025-06-06 09:53:12 +0500

    remove the sticker

--- src/pages/EditFavourites.jsx
+++ src/pages/EditFavourites.jsx
@@ -129,7 +129,7 @@ const EditFavourites = () => {
           onClick={fetchFavourites}
           className="px-6 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-xl shadow-md transition"
         >
-          🔄 Refresh List
+           Refresh List
         </_motion.button>
       </div>
     </div>
