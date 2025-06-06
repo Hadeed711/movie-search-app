59e273057f70c683288aaa4425258905399e33ed
Hadeed711 <hadeedahmad711@gmail.com>
2025-06-06 10:17:51 +0500

    new

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,7 +5,6 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
-    Accept: "application/json",
   },
 });
 
@@ -17,18 +16,5 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
-
-// Add response interceptor to handle 401 errors
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      // Handle unauthorized error (token expired or invalid)
-      localStorage.removeItem("access_token");
-      window.location.href = "/login"; // Redirect to login page
-    }
-    return Promise.reject(error);
-  }
-);
 
 export default api;
