915e41d526963812bb6f02084d6b579141be8fe3
Hadeed711 <hadeedahmad711@gmail.com>
2025-07-02 18:48:49 +0500

    token error

--- src/pages/AIRecommend.jsx
+++ src/pages/AIRecommend.jsx
@@ -13,7 +13,7 @@ function AIRecommend() {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
         },
         body: JSON.stringify({ prompt }),
       });
